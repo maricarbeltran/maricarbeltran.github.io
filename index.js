47ed9f5ee64a25de1fdd42a46b7cf7e2cd630f34
maricarbeltran <ma.maricarbeltran22@gmail.com>
2024-05-25 12:24:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const btnLikes1 = document.getEmentById("id="btnLike1")
-const btnLikes2 = document.getEmentyById("id="btnLike2")
-const countLikes1 = document.getElementyByaid("id="countLike1")
-const countLikes2 = document.getElementyById("id="countLike2")
+const btnLike1 = document.getEmentById("id="btnLike1")
+const btnLike2 = document.getEmentyById("id="btnLike2")
+const btnLike3 = document.getEmentyById("id="btnLike3")
+const btnLike4 = document.getEmentyById("id="btnLike4")
+const countLike1 = document.getElementyById("id="countLike1")
+const countLike2 = document.getElementyById("id="countLike2")
+  const countLike3 = document.getElementyById("id="countLike3")
+  const countLike4 = document.getElementyById("id="countLike4")
+
+  
 
 function hitLikes() {
   let totalLike1 = parseInt(countLikes1.value) + 1
