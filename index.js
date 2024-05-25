f61c7dcfc1ba285867fec19ac4baabaec27b5670
maricarbeltran <ma.maricarbeltran22@gmail.com>
2024-05-25 12:26:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const btnLike1 = document.getEmentById("id="btnLike1")
 const btnLike2 = document.getEmentyById("id="btnLike2")
 const btnLike3 = document.getEmentyById("id="btnLike3")
 const btnLike4 = document.getEmentyById("id="btnLike4")
-const countLike1 = document.getElementyById("id="countLike1")
-const countLike2 = document.getElementyById("id="countLike2")
-  const countLike3 = document.getElementyById("id="countLike3")
-  const countLike4 = document.getElementyById("id="countLike4")
+const countLikes1 = document.getElementyById("id="countLike1")
+const countLikes2 = document.getElementyById("id="countLike2")
+  const countLikes3 = document.getElementyById("id="countLike3")
+  const countLikes4 = document.getElementyById("id="countLike4")
 
   
 
