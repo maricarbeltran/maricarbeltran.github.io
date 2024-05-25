cefbc3bfeb760f00607f2a33bd6c43a26623f3c7
maricarbeltran <ma.maricarbeltran22@gmail.com>
2024-05-25 11:33:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const btn = document.getEmentById("id="btnLike1")
+const btn = document.getEmentyById("id="btnLike2")
+const countLike1 = document.getElementyByaid("id="countLike1")
+const countLike2 = document.getElementyById("id="countLike2")
 
+function hitLikes() {
+  let totalLike1: number = parseInt(countLikes1.value) + 1
+  countLikes1. textContent = totalLikes1.toString() 
+}
